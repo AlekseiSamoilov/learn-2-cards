95e0ae65486e63c6e2e61257bf35c06d7d4c1c7b
Aleksei Samoilov <lek-z@yandex.ru>
2024-10-16 15:24:10 +0200

    fix: rework card.service

--- backend/src/cards/cards.service.ts
+++ backend/src/cards/cards.service.ts
@@ -1,22 +1,29 @@
 import { Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
 import { Card } from "./card.schema";
-import { Repository } from "typeorm";
 import { CreateCardDto } from "./dto/create-card.dto";
 import { ObjectId } from "mongodb";
 import { UpdateCardDto } from "./dto/update-card.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model, Types } from "mongoose";
+import { find } from "rxjs";
+import { Type } from "class-transformer";
+import { Not } from "typeorm";
 
 @Injectable()
 export class CardsService {
     constructor(
-        @InjectRepository(Card)
-        private readonly cardRepository: Repository<Card>
+        @InjectModel(Card.name)
+        private cardModel: Model<Card>
     ) { }
 
-    async create(createCardDto: CreateCardDto): Promise<Card> {
+    async create(createCardDto: CreateCardDto, userId: string, categoryId: string): Promise<Card> {
         try {
-            const newCard = await this.cardRepository.save(createCardDto);
-            return newCard;
+            const newCard = new this.cardModel({
+                ...createCardDto,
+                userId: new Types.ObjectId(userId),
+                categoryId: new Types.ObjectId(categoryId)
+            });
+            return newCard.save();
         } catch (error) {
             throw new InternalServerErrorException(`Failed to create new card: ${error.message}`);
         }
@@ -24,7 +31,7 @@ export class CardsService {
 
     async findAll(): Promise<Card[]> {
         try {
-            return await this.cardRepository.find();
+            return await this.cardModel.find();
         } catch (error) {
             throw new InternalServerErrorException(`Failed to find cards: ${error.message}`);
         }
@@ -32,32 +39,9 @@ export class CardsService {
 
     async findOne(id: string): Promise<Card> {
         try {
-            const card = await this.cardRepository.findOne({ where: { id: new ObjectId(id) } });
+            const card = await this.cardModel.findById(id);
             if (!card) {
                 throw new NotFoundException(`Card with id ${id} not found`);
-            }
-            return card;
-        } catch (error) {
-            if (error instanceof NotFoundException) {
-                throw error;
-            }
-            throw new InternalServerErrorException(`Failed to find card: ${error.message}`);
-        }
-    }
-
-    async findAllByUserId(userId: string): Promise<Card[]> {
-        try {
-            return await this.cardRepository.find({ where: { userId: new ObjectId(userId) } });
-        } catch (error) {
-            throw new InternalServerErrorException(`Failed to find cards for user ${userId}: ${error.message}`);
-        }
-    }
-
-    async findOneByUserId(userId: string, id: string): Promise<Card> {
-        try {
-            const card = await this.cardRepository.findOne({ where: { userId: new ObjectId(userId), id: new ObjectId(id) } });
-            if (!card) {
-                throw new NotFoundException(`Card with id: ${id} for user ${userId} was not find`);
             }
             return card;
         } catch (error) {
@@ -70,12 +54,11 @@ export class CardsService {
 
     async update(id: string, updateCardDto: UpdateCardDto): Promise<Card> {
         try {
-            const card = await this.findOne(id);
-            if (!card) {
+            const updatedCard = await this.cardModel.findByIdAndUpdate(id, { updateCardDto, updatedAt: new Date() }, { new: true });
+            if (!updatedCard) {
                 throw new NotFoundException(`Card with id ${id} not found`);
             }
-            Object.assign(card, updateCardDto)
-            return await this.cardRepository.save(card)
+            return updatedCard
         } catch (error) {
             if (error instanceof NotFoundException) {
                 throw error;
@@ -86,7 +69,7 @@ export class CardsService {
 
     async remove(id: string): Promise<void> {
         try {
-            const result = await this.cardRepository.delete(new ObjectId(id));
+            const result = await this.cardModel.deleteOne({ _id: id });
             if ((result as any).deletedCount === 0) {
                 throw new NotFoundException(`Card with id ${id} not found`);
             }
@@ -96,6 +79,67 @@ export class CardsService {
             }
             throw new InternalServerErrorException(`Failed to remove card ${error.message}`);
         }
+    }
+    async findByCategory(categoryId: string): Promise<Card[]> {
+        try {
+            const findedCards = await this.cardModel.find({ categoryId: new Types.ObjectId(categoryId) });
+            if (!findedCards) {
+                throw new NotFoundException(`Cards in the category with id: ${categoryId}, not found`)
+            }
+            return findedCards
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                throw error
+            }
+            throw new InternalServerErrorException(`Failed to find cards: ${error.message}`);
+        }
+    }
 
+    async incrementTotalShows(id: string): Promise<Card> {
+        try {
+            const updatedCard = await this.cardModel.findByIdAndUpdate(id, { $inc: { totalShows: 1 } }, { new: true });
+            if (!updatedCard) {
+                throw new NotFoundException(`Card wi id: ${id} not found`)
+            }
+            return updatedCard
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                throw error
+            }
+            throw new InternalServerErrorException(`Failed to update shows: ${error.message}`)
+        }
+    }
+
+    async incrementCorrectAnswers(id: string): Promise<Card> {
+        try {
+            const updatedCard = await this.cardModel.findByIdAndUpdate(id, { $inc: { correctAnswers: 1, totalShows: 1 } }, { new: true });
+            if (!updatedCard) {
+                throw new NotFoundException(`Card with id: ${id} not found`);
+            }
+            return updatedCard
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                throw error
+            }
+            throw new InternalServerErrorException(`Failed to update card: ${error.message}`)
+        }
+    }
+
+    async getRandomCard(categoryId: string): Promise<Card> {
+        try {
+            const cards = await this.cardModel.aggregate([
+                { $match: { categoryId: new Types.ObjectId(categoryId) } },
+                { $sample: { size: 1 } }
+            ]);
+            if (cards.length === 0) {
+                throw new NotFoundException(`No cards found in category with id: ${categoryId}`)
+            }
+            return cards[0];
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                throw error
+            }
+            throw new InternalServerErrorException(`Failed to get cards: ${error.message}`)
+        }
     }
 }
--- backend/src/cards/dto/create-card.dto.ts
+++ backend/src/cards/dto/create-card.dto.ts
@@ -1,18 +1,7 @@
-import { IsNotEmpty, IsString } from "class-validator";
-import { ObjectId } from "mongodb";
 
 export class CreateCardDto {
-    @IsString()
-    @IsNotEmpty()
-    frontSide: string;
-
-    @IsString()
-    @IsNotEmpty()
-    backSide: string;
-
-    @IsNotEmpty()
-    categoryId: ObjectId;
-
-    @IsNotEmpty()
-    userId: ObjectId;
+    frontside: string;
+    backside: string;
+    categoryId: string;
+    imageUrl: string;
 }
