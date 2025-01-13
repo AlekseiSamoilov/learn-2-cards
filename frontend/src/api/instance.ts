32a222ee2c34b0669acac6b41b64a456f14888ae
Aleksei Samoilov <lek-z@yandex.ru>
2025-01-13 12:53:06 +0100

    fix: fix instance path

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/';
+const BASE_URL = import.meta.env.PROD
+    ? 'https://api.листай-знай.рф'
+    : 'http://localhost:3000';
 
 const api = axios.create({
     baseURL: BASE_URL,
